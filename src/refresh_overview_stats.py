84e36bcc5499de81a3b2690c1671254992d76d40
Jakub Vasicek <j.vasicek58@gmail.com>
2024-10-17 14:07:26 +1100

    add overview values to gene nodes

--- src/refresh_overview_stats.py
+++ src/refresh_overview_stats.py
@@ -31,25 +31,30 @@ session = DB_connection.session()
 
 print ('Updating overview statistics.')
 
-for gID in all_gene_ids:
+for i,gID in enumerate(all_gene_ids):
     query_str = 'MATCH (g:Gene {id: \'' + gID + '\'}) '
     query_str += "CALL apoc.path.subgraphAll(g, {relationshipFilter:'<TRANSCRIPT_OF|<EXON_PART_OF|<VARIANT_MAPS_TO|<INCLUDES_ALT_ALLELE|INCLUDES_EXON>|HAPLO_FORM_OF|<ENCODED_BY_TRANSCRIPT|<ENCODED_BY_HAPLOTYPE|<MAPS_TO'}) YIELD nodes RETURN nodes, [ v in nodes | labels(v) ] as node_types;"
     query_response = session.run(query_str).data()
 
-    matched_variants = len([ elem for elem in query_response[0]['nodes'] if ('overlapping_peptide' in elem[0]) and (elem[0]['overlapping_peptide']) ])
-    total_proteoforms = len([ elem for elem in query_response[0]['node_types'] if elem[0] == 'Proteoform'])
-    total_peptides = len([ elem for elem in query_response[0]['node_types'] if elem[0] == 'Peptide'])
-    variant_peptides = len([ elem for elem in query_response[0]['nodes'] if ('pep_class_1' in elem) and (('variant' in elem['pep_class_1']) or ('frameshift' in elem['pep_class_1'])) ])
-    
-    time.sleep(0.7)
+    if (len(query_response) > 0):
+        time.sleep(0.7)
 
-    query_str = 'MATCH (g:Gene {id: \'' + gID + '\'}) '
-    query_str += "SET g.matched_vars = " + str(matched_variants) 
-    query_str += " SET g.total_proteoforms = " + str(total_proteoforms) 
-    query_str += " SET g.total_peptides = " + str(total_peptides) 
-    query_str += " SET g.variant_peptides = " + str(variant_peptides) 
-    session.run(query_str)
+        matched_variants = len([ elem for elem in query_response[0]['nodes'] if ('overlapping_peptide' in elem[0]) and (elem[0]['overlapping_peptide']) ])
+        total_proteoforms = len([ elem for elem in query_response[0]['node_types'] if elem[0] == 'Proteoform'])
+        total_peptides = len([ elem for elem in query_response[0]['node_types'] if elem[0] == 'Peptide'])
+        variant_peptides = len([ elem for elem in query_response[0]['nodes'] if ('pep_class_1' in elem) and (('variant' in elem['pep_class_1']) or ('frameshift' in elem['pep_class_1'])) ])
+        
+
+        query_str = 'MATCH (g:Gene {id: \'' + gID + '\'}) '
+        query_str += "SET g.matched_vars = " + str(matched_variants) 
+        query_str += " SET g.total_proteoforms = " + str(total_proteoforms) 
+        query_str += " SET g.total_peptides = " + str(total_peptides) 
+        query_str += " SET g.variant_peptides = " + str(variant_peptides) 
+        session.run(query_str)
+        
     time.sleep(0.7)
+    print('Processed:', i, '/', len(all_gene_ids), end='\r')
+    
 
 session.close()
 print ('Done')
